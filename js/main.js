aa17438333b1d3fc668b971e16dde4378ed12270
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-06 16:03:12 +0530

    javascript console.log test

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-let close = document.getElementById("close-menu-btn")
-let open = document.getElementById("open-menu-btn")
+const close = document.getElementById("close-menu-btn");
+const open = document.getElementById("open-menu-btn");
+
+close.addEventListener("click", closeMenu);
+
+function closeMenu() {
+    console.log("Is this working?")
+}
+
+
 
 
