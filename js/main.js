6155ed8d4a483aa1ab893b4f944d93ae8255a4cd
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-08-06 16:22:44 +0530

    sidebar animation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 const close = document.getElementById("close-menu-btn");
 const open = document.getElementById("open-menu-btn");
+const sidebar = document.getElementById("sidebar");
 
 close.addEventListener("click", closeMenu);
+open.addEventListener("click", openMenu)
 
 function closeMenu() {
+    sidebar.style.display = "none"   
     console.log("Is this working?")
 }
+
+function openMenu() {
+    sidebar.style.display = "flex"
+    console.log("Is this working?")
+}
+
 
 
 
